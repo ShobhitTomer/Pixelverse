fc3f67ee5af6bf28e492589fd7a2c3cafa0f1a92
Shobhit Tomer <113280718+ShobhitTomer@users.noreply.github.com>
2023-10-15 22:11:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu(){
+    sidemenu.style.right = "0";
+}
+
+function closemenu(){
+    sidemenu.style.right = "-200px";
+}
+
+// ================================googleForm========================================
+
+// 
+///SLIDER
+
+var typed = new Typed(".auto-type",{
+    strings:["Video Editing","Storytelling","Content Writing","Graphic Designing"],
+    typeSpeed:80,
+    backSpeed:80,
+    loop:true
+})
+
+new WOW().init();
+
